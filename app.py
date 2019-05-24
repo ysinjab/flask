93de047db230988802f46abb1b9d9f642f1bae3b
Yasser Sinjab <ysinjab@gmail.com>
2019-05-24 20:15:53 +0300

    f

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized 222'
+    return 'Flask Dockerized 111'
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=80)
